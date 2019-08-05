8e855a2d5b14808af37ade18aa05ed9be3eec124
Mykhailo Bondarenko <mcmikius@icloud.com>
2019-08-05 09:58:46 +0300

    init
    code from lessons

---
+++ src/app/app.component.ts
@@ -0,0 +1,61 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements  OnInit {
+
+  title = 'TODO';
+  todosArray = [];
+  newTitle = '';
+  newText = '';
+
+  addNew() {
+    const newItem = {
+      id: 1,
+      status: false,
+      text: this.newText,
+      title: this.newTitle
+    };
+    this.todosArray.unshift(newItem);
+    this.newText = '';
+    this.newTitle = '';
+  }
+
+  deleteItem(text) {
+    const index = this.todosArray.findIndex(el => el.text === text);
+    this.todosArray.splice(index, 1);
+    // alert(text);
+  }
+
+  show() {
+    console.log(this.todosArray)
+  }
+
+  getData() {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then(response => response.json())
+      .then(todos => todos.map((td) => {
+        return {
+          id: td.id,
+          status: td.completed,
+          text: td.title,
+          title: td.reqtitle ? td.reqtitle : `Untitled ${td.id}`
+        };
+      }))
+      .then(todos => {
+        todos.length = 10;
+        return todos;
+      })
+      .then(todos => {
+        this.todosArray = todos;
+      });
+  }
+
+  ngOnInit(): void {
+    this.getData();
+  }
+
+}
